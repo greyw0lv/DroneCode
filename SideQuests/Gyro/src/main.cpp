17279fdb79c5ee23ff63295713eb5cffbe84dfe7
greyw0lv <noahcondotta@gmail.com>
2025-03-20 01:26:20 -0400

    Complete Rework of Attitude Determination
    Gave up on sensor fusion.
    
    Instead using accelerometer gravity vector only.
    
    Finds Roll and Pitch
    
    CANNOT DO YAW

--- SideQuests/Gyro/src/main.cpp
+++ SideQuests/Gyro/src/main.cpp
@@ -1,67 +1,38 @@
 #include <Arduino.h>
 #include "Arduino_BMI270_BMM150.h"
-#include "SimpleKalmanFilter.h"
-#include "SensorFusion.h"
-
 #include <Streaming.h>
+#include <1euroFilter.h>
 
-SimpleKalmanFilter gyroKalmanFilterx(1, 1, 0.01);
-SimpleKalmanFilter gyroKalmanFiltery(1, 1, 0.01);
-SimpleKalmanFilter gyroKalmanFilterz(1, 1, 0.01);
-SimpleKalmanFilter accelKalmanFilterx(2, 1, 0.02);
-SimpleKalmanFilter accelKalmanFiltery(2, 1, 0.02);
-SimpleKalmanFilter accelKalmanFilterz(2, 1, 0.02);
-SimpleKalmanFilter magnetoKalmanFilterx(0.7, 1, 0.01);
-SimpleKalmanFilter magnetoKalmanFiltery(0.7, 1, 0.01);
-SimpleKalmanFilter magnetoKalmanFilterz(0.7, 1, 0.01);
+//stuff for 1 euro
+
+static OneEuroFilter ax;
+static OneEuroFilter ay;
+static OneEuroFilter az;// not enabled yet, setup has to be called later
+
+
+#define FREQUENCY   100   // [Hz] 
+#define MINCUTOFF   3.0   // [Hz] needs to be tuned according to your application
+#define BETA        0.1   // needs to be tuned according to your application
+unsigned long start_time;
+//end of 1 euro
 
 struct threeAxis{
-  float x,y,z;
+  float x,y,z = 0;
 };
-
-threeAxis Gyro;
 threeAxis Accel;
-threeAxis Magneto;
 
-threeAxis Estimated_Gyro;
-threeAxis Estimated_Accel;
-threeAxis Estimated_Magneto;
+threeAxis filtered_Accel;
 
-float pitch, roll, yaw;
-float deltat;
 
-SF fusion; //creates fusion object
-
-auto gyroOffset = [](threeAxis v) {
-  v.x = v.x - -1.0;
-  v.y = v.y -  0.2;
-  v.z = v.z - -0.4;
-  return (v);
-};
-
-auto accelOffset = [](threeAxis v) {
-  v.x = v.x -  0;
-  v.y = v.y -  0;
-  v.z = v.z +  1.0;
-  return (v);
-};
-
-auto accelDeadzone = [](threeAxis v) {
-  if (abs(v.x) < 0.02){v.x = 0;};
-  if (abs(v.y) < 0.02){v.y = 0;};
-  if (abs(v.z) < 0.03){v.z = 0;};
-  return (v);
-};
-
-auto gyroDeadzone = [](threeAxis v) {
-  if (abs(v.x) < 0.13){v.x = 0;};
-  if (abs(v.y) < 0.13){v.y = 0;};
-  if (abs(v.z) < 0.13){v.z = 0;};
-  return (v);
-};
 
 void setup() {
   Serial.begin(9600);
+
+  ax.begin(FREQUENCY, MINCUTOFF, BETA);
+  ay.begin(FREQUENCY, MINCUTOFF, BETA);
+  az.begin(FREQUENCY, MINCUTOFF, BETA);
+  
+  start_time = micros();
   while (!Serial);
   Serial.println("Started");
   //IMU setup
@@ -69,20 +40,6 @@ void setup() {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
-  //Gyro
-  Serial.print("Gyroscope sample rate = ");
-  Serial.print(IMU.gyroscopeSampleRate());
-  Serial.println(" Hz");
-  Serial.println();
-  Serial.println("Gyroscope in degrees/second");
-  Serial.println("X\tY\tZ");
-  //MagnetoMeter
-  Serial.print("Magnetic field sample rate = ");
-  Serial.print(IMU.magneticFieldSampleRate());
-  Serial.println(" Hz");
-  Serial.println();
-  Serial.println("Magnetic Field in uT");
-  Serial.println("X\tY\tZ");
   //Accelerometer
   Serial.print("Accelerometer sample rate = ");
   Serial.print(IMU.accelerationSampleRate());
@@ -96,52 +53,24 @@ void setup() {
 }
 
 void loop() {
+  float elapsed_time = 1E-6 * (micros() - start_time); // in seconds
 
-  if (IMU.gyroscopeAvailable()) {
-    IMU.readGyroscope(Gyro.x, Gyro.y, Gyro.z);
-    Estimated_Gyro.x = gyroKalmanFilterx.updateEstimate(Gyro.x);
-    Estimated_Gyro.y = gyroKalmanFiltery.updateEstimate(Gyro.y);
-    Estimated_Gyro.z = gyroKalmanFilterz.updateEstimate(Gyro.z);
-
-  }
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(Accel.x, Accel.y, Accel.z);
-    Estimated_Accel.x = accelKalmanFilterx.updateEstimate(Accel.x);
-    Estimated_Accel.y = accelKalmanFiltery.updateEstimate(Accel.y);
-    Estimated_Accel.z = accelKalmanFilterz.updateEstimate(Accel.z);
-
+    filtered_Accel.x = ax.filter(Accel.x, elapsed_time);
+    filtered_Accel.y = ay.filter(Accel.y, elapsed_time);
+    filtered_Accel.z = az.filter(Accel.z, elapsed_time);
   }
-  if (IMU.magneticFieldAvailable()) {
-    IMU.readMagneticField(Magneto.x, Magneto.y, Magneto.z);
-    Estimated_Magneto.x = magnetoKalmanFilterx.updateEstimate(Magneto.x);
-    Estimated_Magneto.y = magnetoKalmanFiltery.updateEstimate(Magneto.y);
-    Estimated_Magneto.z = magnetoKalmanFilterz.updateEstimate(Magneto.z);
 
-  }
-  Estimated_Gyro   = gyroOffset(Estimated_Gyro);
-  Estimated_Accel  = accelOffset(Estimated_Accel); //doesnt seem to have 
-  //Estimated_Magneto= (Estimated_Magneto);
 
-  Estimated_Gyro   = gyroDeadzone(Estimated_Gyro);
-  Estimated_Accel  = accelDeadzone(Estimated_Accel);
-  //Estimated_Magneto= deadzone(Estimated_Magneto);
+  Serial << "Accel:" <<"\tX:"<< Accel.x <<"\tY:"<< Accel.y <<"\tZ:"<< Accel.z <<endl;
 
   
-  //Serial << "Gyro: " <<"\tX:"<< Estimated_Gyro.x <<"\tY:"<< Estimated_Gyro.y <<"\tZ:"<< Estimated_Gyro.z <<endl;
-  Serial << "Accel:" <<"\tX:"<< Estimated_Accel.x <<"\tY:"<< Estimated_Accel.y <<"\tZ:"<< Estimated_Accel.z <<endl;
-  //Serial << "Magne:" <<"\tX:"<< Estimated_Magneto.x <<"\tY:"<< Estimated_Magneto.y <<"\tZ:"<< Estimated_Magneto.z <<endl<<endl;
+  float curRoll = map(filtered_Accel.x*100000.0,-1.0f,1.0f,-90.0f,90.0f)/100000.0;//dont fucky with the map
+  float curYaw = map(filtered_Accel.y*100000.0,-1.0f,1.0f,-90.0f,90.0f)/100000.0;//dont fucky with the map
+
+  Serial << "Roll:" << curRoll << "Yaw:" << curYaw << endl;
   
 
-
-  
-  deltat = fusion.deltatUpdate();
-  fusion.MahonyUpdate(Estimated_Gyro.x, Estimated_Gyro.y, Estimated_Gyro.z, Estimated_Accel.x, Estimated_Accel.y, Estimated_Accel.x, Estimated_Magneto.x, Estimated_Magneto.y, Estimated_Magneto.z, deltat);  //mahony is suggested if there isn't the mag
-  //fusion.MadgwickUpdate(Estimated_Gyro.x, Estimated_Gyro.y, Estimated_Gyro.z, Estimated_Accel.x, Estimated_Accel.y, Estimated_Accel.x, Estimated_Magneto.x, Estimated_Magneto.y, Estimated_Magneto.z, deltat);  //else use the magwick
-
-  roll = fusion.getRoll();
-  pitch = fusion.getPitch();
-  yaw = fusion.getYaw();
-
-  //Serial << "Pitch:\t" << pitch << "\t\tRoll:\t" << roll << "\t\tYaw:\t" << yaw << endl;
-  delay(5);
+  delay(50);
 }
